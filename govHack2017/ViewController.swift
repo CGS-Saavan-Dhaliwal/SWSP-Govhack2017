3883686b83beeef6f5e9858943836e5dd8b60ece
Josh <17039@cgs.act.edu.au>
2017-07-28 20:48:08 +1000

    Follow up commit

--- govHack2017/ViewController.swift
+++ govHack2017/ViewController.swift
@@ -1,25 +1,29 @@
 //
 //  ViewController.swift
-//  govHack2017
+//  CoreLocationDemo
 //
-//  Created by Joshua Mengel on 28/7/17.
+//  Created by Joshua Mengel on 25/7/17.
 //  Copyright © 2017 Joshua Mengel. All rights reserved.
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
-
+    
+    var locationManager: CLLocationManager?
+    var startLocation: CLLocation?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    
 }
-
