1f511180d51c31cf9204eab34507bcfd5116d891
Josh <17039@cgs.act.edu.au>
2017-07-28 20:47:47 +1000

    Second changes. Created basic UI

---
+++ govHack2017/ViewController-about.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController-about.swift
+//  govHack2017
+//
+//  Created by Joshua Mengel on 28/7/17.
+//  Copyright © 2017 Joshua Mengel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController_about: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
