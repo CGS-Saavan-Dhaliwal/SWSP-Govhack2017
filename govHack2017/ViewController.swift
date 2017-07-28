f1963205159bd3d347d773748dcc0168ce330b25
Josh <17039@cgs.act.edu.au>
2017-07-28 19:30:30 +1000

    Initial Commit

---
+++ govHack2017/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  govHack2017
+//
+//  Created by Joshua Mengel on 28/7/17.
+//  Copyright © 2017 Joshua Mengel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
